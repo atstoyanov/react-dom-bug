ea49d4e3add43e2c6b41ce16d8697f6541a9c43e
Angel Stoyanov <angel@skyfi.com>
2024-06-11 14:57:00 +0300

    navigation example

--- app/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { TestProvider } from './testProvider';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <TestProvider>{children}</TestProvider>;
-}
